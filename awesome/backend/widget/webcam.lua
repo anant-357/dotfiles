28dd81e6efbc3bea2d079d08fc9a8ad7f2298e05
anant <95141712+anant-357@users.noreply.github.com>
2023-06-10 14:55:14 +0530

    added webcam support(crashes when firefox uses webcam)

---
+++ awesome/backend/widget/webcam.lua
@@ -0,0 +1,36 @@
+local helpers = require("backend.helpers")
+local shell   = require("awful.util").shell
+local wibox   = require("wibox")
+local string  = string
+
+
+local function factory(args)
+    args           = args or {}
+
+    local webcam   = { widget = args.widget or wibox.widget.textbox() }
+    local timeout  = args.timeout or 2
+    local settings = args.settings or function() end
+
+    local cmd      = "fuser -v /dev/video0 &> new.txt; cat new.txt"
+
+    function webcam.update()
+        helpers.async({ shell, "-c", cmd }, function(f)
+            webcam_now = {
+            }
+            if string.match(f, "Specified") then
+                webcam_now.status = "off"
+            elseif string.match(f, "PID") then
+                webcam_now.status = "on"
+            else
+                webcam_now.status = "avail"
+            end
+            settings()
+        end)
+    end
+
+    webcam.timer = helpers.newtimer("webcam", timeout, webcam.update, true, true)
+
+    return webcam
+end
+
+return factory
---
+++ awesome/components/webcam.lua
@@ -0,0 +1,64 @@
+local awful                 = require("awful")
+local wibox                 = require("wibox")
+local functions             = require("functions")
+local wibar_widget_enhancor = functions.wi_widget_enhancor
+local wibar_widget_shape    = functions.wi_widget_shape
+local backend               = require("backend")
+local colors                = require("colorschemes.gruvbox")
+
+
+local function webcam_function(args)
+    local container_bool       = args.container or "no"
+    local webcam_icon          = wibox.widget.textbox("")
+    webcam_icon.font           = "Symbols Nerd Font Mono 10"
+
+    local webcam_containerized = wibar_widget_enhancor(webcam_icon, colors.dark_green)
+
+    local webcam_status        = {}
+    --󰈈󰈉󰛐󰛑
+    local webcam               = backend.widget.webcam({
+        timeout = 1,
+        settings = function()
+            if webcam_now.status == "off" then
+                webcam_icon:set_text("")
+                webcam_icon.font = "Symbols Nerd Font Mono 12"
+            else
+                webcam_icon:set_text("")
+                webcam_icon.font = "Symbols Nerd Font Mono 12"
+            end
+            webcam_status = webcam_now
+        end
+    })
+
+    webcam_icon:buttons(awful.util.table.join(
+        awful.button({}, 1, function()
+            if webcam_status.status == "off" then
+                awful.util.spawn("sudo modprobe uvcvideo")
+            else
+                awful.util.spawn("sudo rmmod -f uvcvideo")
+            end
+        end)
+    ))
+
+    local webcam_t = awful.tooltip {
+        objects = { webcam_icon },
+        bg = colors.background,
+        fg = colors.foreground,
+        shape = wibar_widget_shape,
+        timer_function = function()
+            if webcam_status.status == "off" then
+                return backend.util.markup.font("Fira Code, Medium 10", "Webcam Off")
+            end
+            return backend.util.markup.font("Fira Code, Medium 10", "Webcam On")
+            -- return webcam_status.status
+        end
+
+    }
+    if container_bool == "yes" then
+        return webcam_containerized
+    else
+        return webcam_icon
+    end
+end
+
+return webcam_function
