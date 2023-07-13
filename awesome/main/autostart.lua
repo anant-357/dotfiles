6715be994503ebb3ef519d738aae2c2c844b27d3
anant-357 <anantsh.88@gmail.com>
2023-07-13 23:00:59 +0530

    redshift

--- awesome/main/autostart.lua
+++ awesome/main/autostart.lua
@@ -1,10 +1,16 @@
 local awful = require("awful")
-
+require("components.brightness")
+require("components").volume_bar({
+    shape = "default",
+    colorscheme = "default"
+})
+awesome.emit_signal("volume_change")
 
 awful.spawn.easy_async("pkill picom")
-require("components.brightness")
 
-require("components").volume_bar({ shape = "default", colorscheme = "default" })
-awful.spawn.easy_async_with_shell("pamixer --unmute ", function() end)
-awesome.emit_signal("volume_change")
-awful.spawn.easy_async_with_shell("picom --config ~/dotfiles/picom/picom.conf ", function() end)
+awful.spawn.easy_async("kill -9 $(pidof redshift)")
+
+awful.spawn.easy_async_with_shell("picom --config ~/dotfiles/picom/picom.conf ", function()
+end)
+
+awful.spawn.easy_async("redshift -l 26.08:96.51 -b 0.9:0.7 -t 5800K:5000K -g 0.8")
